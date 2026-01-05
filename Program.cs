08b2cdae2dbf77d48e2e089fe27dd4d502630658
asadibrahim206 <asad.denin@gmail.com>
2026-01-05 09:03:58 +0500

    learning oops in c#

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+    
+    namespace myProgram
+{
+    class Program
+    {   
+        static void Main()
+        {   
+            
+            const int TotalStudents = 5;
+            int[] score = new int[TotalStudents];
+           
+            
+            for(int i = 0; i < score.Length; i++)
+            {
+                Console.WriteLine("---enter score of student 1 ---");
+                score[i] = int.Parse(Console.ReadLine());
+
+                char grade = CalculateGrade(score[i]);
+                Console.WriteLine($"STUDENT GRADE {grade}");
+            }
+                
+        }
+        static char CalculateGrade(int score)
+        {
+            if (score >= 90) 
+                return 'A';
+            else if (score >= 80 )
+                return 'B';
+            else if (score >= 70)
+                return 'C';
+            else 
+                return 'F' ;
+        }
+      
+        
+    }
+}
+
+
